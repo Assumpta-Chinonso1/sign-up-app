ee6c53aadd6502e75b0a5aa7a8e560ddf4604da1
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-07-15 11:38:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SignUpForm from './SignUpForm/SignUpForm'
+
+const App = () => {
+  return (
+    <div>
+      
+      <SignUpForm/>
+    </div>
+  )
+}
+
+export default App
